4e1e17e664f567abb98aecb2f29704e042a785e1
Neil Stevens <stevensn99@gmail.com>
2021-10-09 15:20:40 +0100

    add test to service to verify if input is book then get expected output

--- Smc.OrdersApi.Business.Tests/Services/PaymentProcessorServiceTests.cs
+++ Smc.OrdersApi.Business.Tests/Services/PaymentProcessorServiceTests.cs
@@ -3,6 +3,7 @@ using Smc.OrdersApi.Business.Domain;
 using Smc.OrdersApi.Business.Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -31,6 +32,35 @@ namespace Smc.OrdersApi.Business.Tests.Services
             var sut = new PaymentProcessorService(null);
 
             await Assert.ThrowsAsync<ArgumentNullException>(() => sut.ProcessPayment(null));
+        }
+
+        [Fact]
+        public async Task ProcessPayment_WhenModelIsBook_Returns_CorrectModel()
+        {
+            var rules = new List<IPaymentRule>()
+            {
+                new PhysicalProductPaymentRule(),
+                new BookProductPaymentRule(),
+                new MembershipPaymentRule(),
+                new VideoRegulationPaymentRule()
+            };
+
+            var mockInput = new PaymentInputModel
+            {
+                Type = ProductType.Book
+            };
+
+            var sut = new PaymentProcessorService(rules);
+
+            var result = await sut.ProcessPayment(mockInput);
+
+            result.ShouldNotBeNull();
+            result.GenerateCommissionPayment.ShouldBeTrue();
+            result.PackingSlips.ShouldNotBeNull();
+            result.PackingSlips.Count().ShouldBe(2);
+            result.PackingSlips.First().Type.ShouldBe(PackingSlipType.Shipping);
+            result.PackingSlips.Last().Type.ShouldBe(PackingSlipType.Royalty);
+            result.Membership.ShouldBeNull();
         }
     }
 }
